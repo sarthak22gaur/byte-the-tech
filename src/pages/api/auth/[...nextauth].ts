b798669368ca63c67aa39fc84f78f147a9da667a
sarthakgaur22 <sarthakgaur22@gmail.com>
2022-09-06 03:34:47 -0500

    removed email login option

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,11 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 
-// Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-
   callbacks: {
     async session({ session, user, token }) {
       if (session.user) {
@@ -22,29 +17,13 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // Configure one or more authentication providers
+
   adapter: PrismaAdapter(prisma),
   providers: [
-    EmailProvider({
-      server: {
-        host: env.MAILTRAP_HOST,
-        port: env.MAILTRAP_PORT,
-        auth: {
-          user: env.MAILTRAP_USER,
-          pass: env.MAILTRAP_PASS,
-        },
-      },
-      from: env.MAILTRAP_SENDER,
-    }),
-    // DiscordProvider({
-    //   clientId: env.DISCORD_CLIENT_ID,
-    //   clientSecret: env.DISCORD_CLIENT_SECRET,
-    // }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET
     }),
-    // ...add more providers here
   ],
 };
 
